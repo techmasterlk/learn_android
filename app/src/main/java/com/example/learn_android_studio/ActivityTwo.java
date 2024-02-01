153dfa9419f5ea20fcf5c3c0a4e4c88d43a6691a
Irosha Hewage <122591202+iroshahewage@users.noreply.github.com>
2024-02-01 13:42:02 +0530

    init

---
+++ app/src/main/java/com/example/learn_android_studio/ActivityTwo.java
@@ -0,0 +1,41 @@
+package com.example.learn_android_studio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        Button btn2 = findViewById(R.id.btn2);
+        TextView txtUser = findViewById(R.id.userName);
+        TextView txtAge = findViewById(R.id.age);
+
+        Intent getIntent = getIntent();
+
+        Bundle data = getIntent.getExtras();
+
+        String userName = data.getString("USERS", "Guest") ;
+        int age = data.getInt("AGES", 18);
+
+        txtUser.setText(userName);
+        txtAge.setText(String.valueOf(age));
+
+
+        btn2.setOnClickListener((v) -> {
+
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/learn_android_studio/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.learn_android_studio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // get button reference
+        Button btn = findViewById(R.id.btn);
+
+        // setOnClickListener
+        btn.setOnClickListener((v) -> {
+
+            Intent i = new Intent(this, ActivityTwo.class);
+
+            Bundle data = new Bundle();
+
+            data.putString("USER", "Irosha");
+            data.putInt("AGE", 25);
+
+            i.putExtras(data);
+            startActivity(i);
+
+        });
+    }
+}
