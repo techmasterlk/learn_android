a483a4c60eb3edaa242bda1b3fb6e9ecbab0034c
Irosha Hewage <122591202+iroshahewage@users.noreply.github.com>
2024-02-02 09:52:12 +0530

    how work implicit intent

--- app/src/main/java/com/example/learn_android_studio/MainActivity.java
+++ app/src/main/java/com/example/learn_android_studio/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.learn_android_studio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         // get button reference
         Button btn = findViewById(R.id.btn);
+        Button emailBtn = findViewById(R.id.emailBtn);
 
         // setOnClickListener
         btn.setOnClickListener((v) -> {
@@ -32,5 +36,26 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
 
         });
+
+        // send email implicit intent
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"irosh@example.com"} );
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject bro");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "this is Body");
+        emailIntent.setType("text/plain");
+
+        emailBtn.setOnClickListener((v) -> {
+
+           try {
+               startActivity(emailIntent);
+           }catch (ActivityNotFoundException e) {
+               System.out.println(e);
+           }
+
+        });
+
+
     }
 }
